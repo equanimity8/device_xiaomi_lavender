82703820208c18ace8f3888156c4b8327adf90cd
equanimity8 <86476791+equanimity8@users.noreply.github.com>
2021-06-25 21:25:14 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,6 +17,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/revengeos_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_lavender-user \
-    revengeos_lavender-userdebug \
-    revengeos_lavender-eng
+    awaken_lavender-user \
+    awaken_lavender-userdebug \
+    awaken_lavender-eng
